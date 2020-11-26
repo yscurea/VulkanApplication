7b07bb91b3173170000363c08c5dbb242062ea5d
yuusuine <yuuki.990510@gmail.com>
2020-11-27 00:10:20 +0900

    divide device class for device class and physical device

--- AppBase/Device.h
+++ AppBase/Device.h
@@ -7,7 +7,6 @@
 #include "PhysicalDevice.h"
 
 #include "DeviceQueueIndices.h"
-#include "SwapchainSupportDetails.h"
 
 class Device {
 private:
@@ -16,31 +15,10 @@ private:
 	VkDevice device;
 	// デバイス拡張
 	std::vector<const char*> device_extensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
-	// MSAA
-	VkSampleCountFlagBits sample_count_falg_bits;
-	// グラフィックスキュー
-	VkQueue graphics_queue;
-	// グラフィックスキューのインデックス
-	std::optional<uint32_t> graphics_queue_index;
-	// プレゼントキュー
-	VkQueue present_queue;
-	// プレゼントキューのインデックス
-	std::optional<uint32_t> present_queue_index;
 public:
-	// GPUを選択する
-	void selectPhysicalDevice();
-	// GPUが最適かどうか確認する
-	bool isDeviceSuitable(VkPhysicalDevice physical_device);
-	// デバイス拡張が対応しているかどうか確認する
-	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 	// 論理デバイスを生成する
 	void createLogicalDevice();
 	// 論理デバイスを破棄する
 	void deleteLogicalDevice();
-	// 選択されているGPUのキューを取得する
-	DeviceQueueIndices findDeviceQueue(VkPhysicalDevice physical_device);
 
-
-
-	SwapchainSupportDetails querySwapchainSupport(VkPhysicalDevice device);
 };
--- AppBase/PhysicalDevice.h
+++ AppBase/PhysicalDevice.h
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <optional>
+
 class PhysicalDevice {
 	// GPU
 	VkPhysicalDevice physical_device;
 	VkQueue present_queue;
+	std::optional<uint32_t> present_queue_index;
+
 	VkQueue graphics_ququq;
+	std::optional<uint32_t> graphics_queue_index;
+
 	VkQueue compute_queue;
+	std::optional<uint32_t> compute_queue_index;
+
+	// MSAA
+	VkSampleCountFlagBits sample_count_falg_bits;
+public:
+	// GPUを選択する
+	void selectPhysicalDevice();
+	// GPUが最適かどうか確認する
+	bool isDeviceSuitable(VkPhysicalDevice physical_device);
+	// デバイス拡張が対応しているかどうか確認する
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 };
