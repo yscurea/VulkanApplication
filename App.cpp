53a205d0be705dcdfc5a3f6ba31ce7d2b65308cd
yuusuine <yuuki.990510@gmail.com>
2020-11-27 13:49:01 +0900

    format code

--- App.cpp
+++ App.cpp
@@ -596,7 +596,7 @@ static VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFor
 		if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
 			return availableFormat;
 		}
-}
+	}
 
 	return available_formats[0];
 }
