fb6030ea09acf81ced9d1f8e6c3f78230df9699b
yuusuine <yuuki.990510@gmail.com>
2020-11-28 17:47:23 +0900

    update uniform buffer object

--- utils/UniformBufferObject.h
+++ utils/UniformBufferObject.h
@@ -3,7 +3,9 @@
 #include <glm/glm.hpp>
 
 struct UniformBufferObject {
-	alignas(16) glm::mat4 model;
-	alignas(16) glm::mat4 view;
-	alignas(16) glm::mat4 proj;
+	glm::mat4 model;
+	glm::mat4 view;
+	glm::mat4 proj;
+	glm::mat4 depth_bias_MVP;
+	glm::vec3 light_position;
 };
