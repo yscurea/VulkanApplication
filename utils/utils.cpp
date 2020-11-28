a6f50f9d6b87f1739d9039082b7f9bfb06c0faa8
yuusuine <yuuki.990510@gmail.com>
2020-11-28 21:58:39 +0900

    update utils

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -2,4 +2,12 @@
 
 
 namespace vulkan::utils {
+	VkPipelineShaderStageCreateInfo getShaderStageCreateInfo(VkShaderModule shader_module, VkShaderStageFlagBits flag) {
+		VkPipelineShaderStageCreateInfo shader_stage_create_info{};
+		shader_stage_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		shader_stage_create_info.stage = flag;
+		shader_stage_create_info.module = shader_module;
+		shader_stage_create_info.pName = "main";
+		return shader_stage_create_info;
+	}
 }
--- utils/utils.h
+++ utils/utils.h
@@ -3,12 +3,5 @@
 #include <vulkan/vulkan.h>
 
 namespace vulkan::utils {
-	VkCommandBuffer beginSingleTimeCommands();
-	void endSingleTimeCommands(VkCommandBuffer commandBuffer);
-
-	void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
-	void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
-
-
-	void createImage();
+	VkPipelineShaderStageCreateInfo getShaderStageCreateInfo(VkShaderModule shader_module, VkShaderStageFlagBits flag);
 }
